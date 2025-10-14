14efa49feda1f4045f23c69ffa7df88d8bfac210
Reyad Mohamed Fathy <reyadmohamed631@gmail.com>
2025-10-14 14:33:50 +0300

    Comment out server setup and route definitions in app.js for future refactoring

--- app.js
+++ app.js
@@ -1,26 +1,26 @@
-import express from "express";
-import dotenv from "dotenv";
-dotenv.config();
-import databaseConnection from "./config/databaseConnection.js";
-import authRoutes from "./routes/authRoutes.js";
-import userRoutes from "./routes/userRoutes.js";
-import propertyRoutes from "./routes/propertyRoutes.js";
+// import express from "express";
+// import dotenv from "dotenv";
+// dotenv.config();
+// import databaseConnection from "./config/databaseConnection.js";
+// import authRoutes from "./routes/authRoutes.js";
+// import userRoutes from "./routes/userRoutes.js";
+// import propertyRoutes from "./routes/propertyRoutes.js";
 
-const app = express();
-app.use(express.json());
+// const app = express();
+// app.use(express.json());
 
-// database connection
-databaseConnection();
+// // database connection
+// databaseConnection();
 
-// routes
-app.use("/api/auth", authRoutes);
-app.use("/api/user", userRoutes);
-app.use("/api/property", propertyRoutes);
+// // routes
+// app.use("/api/auth", authRoutes);
+// app.use("/api/user", userRoutes);
+// app.use("/api/property", propertyRoutes);
 
-const Port = process.env.PORT || 3001;
+// const Port = process.env.PORT || 3001;
 
-app.listen(Port, () => {
-  console.log(`server is running on port ${Port}`);
-});
+// app.listen(Port, () => {
+//   console.log(`server is running on port ${Port}`);
+// });
 
-export default app;
+// export default app;
